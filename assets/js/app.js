ccffe2c9781f199e59bcf2409d3d573b0267dc26
lofisoul <jason.nolfi@gmail.com>
2017-03-13 20:09:56 -0500

    Reupload without excess files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,16 @@
-  // Global variables
+// Global variables
   //=====================================================
   var topics = ["happy", "sad", "chilled out", "angry", "excited"];
   var widgetWidth = 300;
   var widgetHeight = 380;
   var widgetUrl = "https://embed.spotify.com/?uri=spotify:user:";
-  var artists = []; //NOTE--this creates an empty array that populates with each mood click.  I'm not sure what our 
+  var artists = []; //NOTE--this creates an empty array that populates with each mood click.  I'm not sure what our
   //end plan is for selection/displaying the actual B.I.T. info so creating this as one option
 
   // Functions
   //===============================================
 
-  // Function for displaying topic buttons 
+  // Function for displaying topic buttons
   function renderButtons() {
       $("#item-buttons").empty();
       //loop for adding buttons modeled after in class example on movie topics
@@ -50,7 +50,7 @@
 
       $.ajax({
           url: queryUrl,
-          //headers: { "Authorization": "Bearer " + "BQBVgnFxqm_8fUIR3uRjY5KZTL85HDbuM34jCM8_Sr7W51L8DphLMCNP76c4h_z294Tez3Iz6OerYYbxigMOBDRcOhS5arxkXjxm7j8xEtKrEiMFjHQMPL7GmvBP__JwLyoIuouUD6_0o66TvAb1Upkt7tV4HCwL-C9k"}, 
+          //headers: { "Authorization": "Bearer " + "BQBVgnFxqm_8fUIR3uRjY5KZTL85HDbuM34jCM8_Sr7W51L8DphLMCNP76c4h_z294Tez3Iz6OerYYbxigMOBDRcOhS5arxkXjxm7j8xEtKrEiMFjHQMPL7GmvBP__JwLyoIuouUD6_0o66TvAb1Upkt7tV4HCwL-C9k"},
           method: "GET"
       }).done(function(response) {
 
@@ -64,10 +64,8 @@
               pName.text("Playlist Name: " + results[i].name);
               pHref = $("<p>");
               pHref.text("External Href from Object: " + results[i].external_urls.spotify)
-
               playlistDiv.append(pName);
               playlistDiv.append(pHref);
-
               playlistDiv.addClass("playlistDiv");
               $("#playlist-items").append(playlistDiv);
           } // end of for loop
@@ -126,17 +124,17 @@
           } // end of for loop
           */
 
-      }); // end of done function response  
+      }); // end of done function response
   }); // end of topicItem on click function
 
-  // See note in global variables section regarding array.  Not sure what our design plan is for displaying the event info.  
+  // See note in global variables section regarding array.  Not sure what our design plan is for displaying the event info.
   // For now, to get our "proof of concept", I am just using an onclick event below for each artist name to pull from B.I.T.
   $("#playlist-items").on("click", ".artist-name", function() {
           var artistName = $(this).attr("data-name");
           $("#artist-events").empty();
           console.log(artistName)
 
-          var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=MoodMash"; // to add date range format is "&date=2017-03-01,2017-12-31"; 
+          var queryUrl = "https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=MoodMash"; // to add date range format is "&date=2017-03-01,2017-12-31";
           $.ajax({
                   url: queryUrl,
                   method: "GET"
@@ -159,7 +157,7 @@
                       eventDiv.append(pDate, pVenue, pCity, pCoordinates);
                       $("#artist-events").append(eventDiv);
                   } // end of for loop
-              }) // end of done function 
+              }) // end of done function
       }) // end of onclick for getArtistEvents
 
   // Main process to render initial buttons
@@ -218,37 +216,26 @@
       databaseURL: "",
       storageBucket: ""
   };
-
   firebase.initializeApp(config);
-
   // Create a variable to reference the database
   var database = firebase.database();
-
   // Initial Values
   // var user = "";
-
   database.ref().on("value", function(snapshot) {
-
       //RETURNING USERS
-
       $("#check-user").on("click", function(event) {
           event.preventDefault();
-
           var userId = $("#user-name-input").val().trim();
-
-          //do we need to store to check if exists? and delete the repeat If so? 
+          //do we need to store to check if exists? and delete the repeat If so?
           //In that case:
-
           // database.ref().set({
           //      user-id: user-id,
           //    });
           if (snapshot.child("user-id").exists()) {
-
               //delete recent addition
               //show span of "welcome Back"
               //trigger function to change html to show mood icons and home page.
               //load user information with stored zipcode
-
           } else {
               //delete recent addition
               //show span above input "We did not recognize this name"
@@ -269,3 +256,83 @@
       //trigger function to change html to show mood icons and home page.
       //end of firebase on value
   });
+
+
+
+
+//KEEP THIS CODE to MERGE
+/*         $.ajax({
+          url: queryUrl,
+          // headers: { "Authorization": "Bearer " + "BQBVgnFxqm_8fUIR3uRjY5KZTL85HDbuM34jCM8_Sr7W51L8DphLMCNP76c4h_z294Tez3Iz6OerYYbxigMOBDRcOhS5arxkXjxm7j8xEtKrEiMFjHQMPL7GmvBP__JwLyoIuouUD6_0o66TvAb1Upkt7tV4HCwL-C9k"},
+          method: "GET"
+          }).done(function(response) {
+
+           results=response.playlists.items;
+
+           console.log(results);
+
+            for (var i=0; i<results.length; i++) {
+              playlistDiv=$("<div>");
+              $(playlistDiv).attr('data-href',results[i].tracks.href);
+              pName=$("<p>");
+              pName.text("Playlist Name: " + results[i].name);
+              pHref=$("<p>");
+              pHref.text("External Href from Object: " + results[i].external_urls)
+
+              playlistDiv.append(pName);
+              playlistDiv.append(pHref);
+
+              playlistDiv.addClass("playlistDiv");
+              $("#playlistItems").append(playlistDiv);
+            } // end of for loop
+          });  // end of done function response
+    }); // end of topicItem on click function
+
+    //GET https://api.spotify.com/v1/users/{user_id}/playlists/{playlist_id}/tracks
+    */
+
+//THIS FUNCTION GRABS INFO FROM RETURNED LISTING
+function grabPlaylist(e) {
+  e.preventDefault();
+  var playlistUrl = $(this).attr('data-href');
+  $.ajax({
+
+  url: playlistUrl,
+  headers: { "Authorization": "Bearer " + access_token},
+   method: "GET"
+   }).done(function(response) {
+     console.log(response);
+     for (i=0; i<response.items.length; i++) {
+     console.log('Artist: ' + response.items[i].track.artists[0].name);
+     console.log('Song: ' + response.items[i].track.name);
+   }
+  });
+}
+
+//THIS GETS THE ACCESS TOKEN DURING SESSION
+function getTokenFromServer() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+  q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+    hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
+
+//RENDER TOKEN
+var token = getTokenFromServer();
+var access_token = token.access_token;
+console.log(token);
+
+//firebased god
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCJWSKJq1r2_Fyu9hk8NdNFXWV1PAmgLXU",
+    authDomain: "mood-mash-userbase.firebaseapp.com",
+    databaseURL: "https://mood-mash-userbase.firebaseio.com",
+    storageBucket: "mood-mash-userbase.appspot.com",
+    messagingSenderId: "1081624019575"
+  };
+
+$('#playlistItems').on('click', '.playlistDiv', grabPlaylist);
