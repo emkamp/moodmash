9fbb10ab9490a091d07c4420b1d594215cc9d999
emkamp <emily.pulskamp@gmail.com>
2017-03-18 17:30:46 -0500

    lots of style work.  events div now disappears when user closes the last event. gradients work, just need more pairs of classes to go with each family of moods. restructured html. sometimes star click effect doesn't fire.  big weird errors in console involving ad servers???

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,8 +33,25 @@ var inTownEvents = [];
 var userCity = '';
 var genPlaylist = [];
 
+// -----------------------------------------------------------------   UI  ------
+// ------------------------------------------------------------------------------
+
+
+function shift() {
+    $("#shifty").addClass("shifty-chill-1");
+    $("#shifty-layer").addClass("shifty-chill-2");
+
+    window.setInterval(function() {
+        $("#shifty-layer").fadeOut(3000).fadeIn(3000);
+    }, 500);
+}
+
+shift();
+
 // ------------------------------------------------------------ FUNCTIONS ------
 //------------------------------------------------------------------------------
+
+
 
 function cityLaunch(e) {
     e.preventDefault();
@@ -79,7 +96,7 @@ function genPlaylists(e) {
     e.preventDefault();
     //JN: topic should be OK to be locally scoped
     var topic = $(this).attr("data-emo");
-    console.log("genPlaylists: topic = " + topic);
+    //console.log("genPlaylists: topic = " + topic);
     var limit = 5 //Set Max results -> Could be an option FEATURE
     var queryUrl = "https://api.spotify.com/v1/search?q=" + topic + "&type=playlist&limit=" + limit;
     $.ajax({
@@ -89,18 +106,18 @@ function genPlaylists(e) {
         method: "GET"
     }).done(function(response) {
         results = response.playlists.items;
-        console.log("genPlaylists results -->");
-        console.log(results);
+        //console.log("genPlaylists results -->");
+        //console.log(results);
         var max = 5;
         var min = 1;
         var num = parseInt(Math.random() * (max - min) + min);
         genPlaylist = results[num];
-        console.log("genPlaylist -->")
-        console.log(genPlaylist);
+        //console.log("genPlaylist -->")
+        //console.log(genPlaylist);
         var href = genPlaylist.href;
-        console.log("genPlaylist.href = " + href);
+        //console.log("genPlaylist.href = " + href);
         var uri = genPlaylist.uri;
-        console.log("uri = " + uri);
+        //console.log("uri = " + uri);
         grabArtist(href);
         launchPlayer(uri, href);
     }); // end of done function response
@@ -128,15 +145,15 @@ function reupSpotify() {
 }
 
 function grabArtist(url) {
-    console.log("grabArtist(url): " + url);
+    //console.log("grabArtist(url): " + url);
     $.ajax({
         context: this,
         url: url,
         headers: { "Authorization": "Bearer " + access_token },
         method: "GET"
     }).done(function(response) {
-        console.log("grabArtist ajax call -->");
-        console.log(response);
+        //console.log("grabArtist ajax call -->");
+        //console.log(response);
         for (i = 0; i < response.tracks.items.length; i++) {
             var artist = response.tracks.items[i].track.artists[0].name;
             artists.push(artist);
@@ -151,29 +168,33 @@ function grabArtist(url) {
         reupSpotify();
     });
 
-    console.log("thinArr from grabArtist(): -->");
-    console.log(thinArr);
+    //console.log("thinArr from grabArtist(): -->");
+    //console.log(thinArr);
 
     searchShows(thinArr);
 }
 
 function searchShows(arr) {
-    console.log("searchShows has been called");
-    console.log("What's the array in searchShows? -->");
-    console.log(arr);
-    console.log(" - - - - - - - - - - - - - - - - - -");
+    //console.log("searchShows has been called");
+    //console.log("What's the array in searchShows? -->");
+    //console.log(arr);
+    //console.log(" - - - - - - - - - - - - - - - - - -");
     $('#artist-events').append('<h4>Upcoming Shows in ' + userCity + '</h4>');
-    //console.log("searchShows i is: " + arr[i]);
+
+    var localEvents = []
 
     //looping over thinned out artist array
     for (i = 0; i < arr.length; i++) {
         var queryUrl = "https://rest.bandsintown.com/artists/" + arr[i] + "/events?app_id=MoodMash"; // to add date range format is "&date=2017-03-01,2017-12-31";
+
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).done(function(response) {
             eventListings = response;
-            console.log("EVENT LISTINGS - - - - - - - - - - - - - - - - - - - -");
+            //console.log("EVENT LISTINGS: EVERY SHOW FOR THESE ARTISTS - - - - - - - - - - - - - - - - - - - -");
+            //console.log("eventListings.length = " + eventListings.length);
+            //console.log(eventListings);
 
             for (i = 0; i < eventListings.length; i++) {
 
@@ -183,31 +204,67 @@ function searchShows(arr) {
                     var venue = eventListings[i].venue.name;
 
                     console.log("searchShows: " + artistName + " @ " + venue + ", on " + convertedDate);
-                    
-                    var artistDiv = $('<div class="event-div well"><div class="btn-close pull-right"><i class="glyphicon glyphicon-remove"></i></div><div class="btn-save pull-right"><i class="glyphicon glyphicon-star"></i></div></div>');
-                    //JN: May want to structure this more
-                    $(artistDiv).append('<h4 class="event-artist">' + artistName + '</h4><p class="event-venue">' + venue + '</p><p class="event-date">' + convertedDate + '</p>');
-                    $('#artist-events').append(artistDiv);
+
+                    var eventArr = {
+                        'artistName': artistName,
+                        'convertedDate': convertedDate,
+                        'venue': venue,
+                    }
+
+                    //console.log("eventArr  = = = = = = >");
+                    //console.log(eventArr);
+
+                    //localEvents.push(eventArr);
+                    //console.log("localEvents = = = = = >");
+                    //console.log(localEvents);
+
+                    if (localEvents.length > -1) {
+                        $("#suggestions").show();
+                        var artistDiv = $('<div class="event-div"><div class="btn-close pull-right"><i class="glyphicon glyphicon-remove"></i></div><div class="btn-save pull-right"><i class="glyphicon glyphicon-star"></i></div></div>');
+                        $(artistDiv).append('<h4 class="event-artist">' + artistName + '</h4><p class="event-venue">' + venue + '</p><p class="event-date">' + convertedDate + '</p>');
+                        $('#artist-events').append(artistDiv);
+                    } else {}
                 } else {}
             }
+
+            // ------------------------------------------------------- Events Events ---
+            // These need to be here and not events, because these elements do not exist on initial page load, they are created by this function.
             $(".btn-close").on('click', function() {
-                console.log("close button clicked!");
-                $(this).closest(".event-div").slideUp();
+                $(this).closest(".event-div").slideUp(400, function() {
+                    $(this).remove();
+                });
+                if ($('#artist-events').children().length < 3) {
+                    setTimeout(500, noEvents());
+                } else {}
+                console.log("How many children does #artist-events have?");
+                console.log($("#artist-events").children().length);
             });
-            $(".btn-save").on('click', function(){
+
+            $(".btn-save").on('click', function() {
                 $(this).toggleClass("btn-saved");
             });
+
         }); // end of done function
     }
 }
 
+
+function noEvents() {
+    $("#suggestions").fadeOut();
+}
+
+
+
 // ------------------------------------------------------------------ EVENTS -----
 //--------------------------------------------------------------------------------
 
-$("#app-login").hide()
-$("#btn-current-feeling").hide();
+$("#app-login").hide();
 $("#app-main").hide();
-$("#music").hide();
+$("#player").hide();
+$("#suggestions").hide();
+$("#city-mood").hide();
+
+$("#btn-current-feeling").hide();
 
 renderButtons();
 
@@ -216,7 +273,8 @@ $('#emotions').on('click', '.btn-feeling', genPlaylists);
 
 $(".btn-feeling").on("click", function() {
 
-    $("#music").show();
+    $("#player").show();
+    $("#city-mood").show();
     $("#emotions").hide();
 
     $("#artist-events").empty();
@@ -230,14 +288,13 @@ $(".btn-feeling").on("click", function() {
     artists = [];
     topic = $(this).attr("data-emo");
     var listArr = [];
-    // leaving this as playlist instead of artist because jason got the spotify authorization
     var queryUrl = "https://api.spotify.com/v1/search?q=" + topic + "&type=playlist";
 
     $("#moodDiv").empty();
     $("#moodDiv").append(this.getAttribute("data-emo"));
     $("#playlist-items").empty();
 
-    //TH NOTE- BEGINNNIG of Firebase stuff ====================================
+    //TH NOTE- BEGINNNIG of Firebase stuff
     var moodRef = firebase.database();
     var mood = this.getAttribute("data-emo");
 
@@ -251,16 +308,21 @@ $(".btn-feeling").on("click", function() {
         var cityMood = (snapshot.val().userMood + " | "); // TH note--all that empty space is to put space between words. I'm sure there's a more elgant way but going for quick and dirty
         $("#recent-moods").html(cityMood);
     });
-    //TH NOTE --end of firebase stuff =========================================
+    //TH NOTE --end of firebase stuff
 });
+
+// ------------------------------------------------ Select a new feeling ---
 
 $("#btn-current-feeling").on('click', function() {
     //thinArr = [];
     $(this).hide();
-    $("#music").hide();
+    $("#player").hide();
+    $("#suggestions").hide();
+    $("#recent-moods").hide();
     $("#emotions").show();
     $("#artist-events").empty();
 });
+
 
 // ------------------------------------------------------------ FIREBASE -------
 //------------------------------------------------------------------------------
